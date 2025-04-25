15945b2631d496075ac79114e10adca0919594dd
Michael Hofweller <michael.hofweller@gmail.com>
2025-04-25 09:42:17 -0700

    supabase callback auth route testing

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,24 +1,27 @@
-import { createClient } from "@/utils/supabase/server";
-import { NextResponse } from "next/server";
+import { createClient } from '@/utils/supabase/server';
+import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-  // The `/auth/callback` route is required for the server-side auth flow implemented
-  // by the SSR package. It exchanges an auth code for the user's session.
-  // https://supabase.com/docs/guides/auth/server-side/nextjs
-  const requestUrl = new URL(request.url);
-  const code = requestUrl.searchParams.get("code");
-  const origin = requestUrl.origin;
-  const redirectTo = requestUrl.searchParams.get("redirect_to")?.toString();
+    // The `/auth/callback` route is required for the server-side auth flow implemented
+    // by the SSR package. It exchanges an auth code for the user's session.
+    // https://supabase.com/docs/guides/auth/server-side/nextjs
+    const requestUrl = new URL(request.url);
+    const code = requestUrl.searchParams.get('code');
+    const origin = requestUrl.origin;
+    const redirectTo = requestUrl.searchParams.get('redirect_to')?.toString();
+    console.log('Request URL:', requestUrl);
+    console.log('Origin:', requestUrl.origin);
+    console.log('Redirect to:', requestUrl.searchParams.get('redirect_to'));
 
-  if (code) {
-    const supabase = await createClient();
-    await supabase.auth.exchangeCodeForSession(code);
-  }
+    if (code) {
+        const supabase = await createClient();
+        await supabase.auth.exchangeCodeForSession(code);
+    }
 
-  if (redirectTo) {
-    return NextResponse.redirect(`${origin}${redirectTo}`);
-  }
+    if (redirectTo) {
+        return NextResponse.redirect(`${origin}${redirectTo}`);
+    }
 
-  // URL to redirect to after sign up process completes
-  return NextResponse.redirect(`${origin}/protected`);
+    // URL to redirect to after sign up process completes
+    return NextResponse.redirect(`${origin}/protected`);
 }
